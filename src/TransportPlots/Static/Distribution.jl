490ccfc94c36e043b0e837fee2232a9fd9201d92
Christopher Everett <cn.everett@outlook.com>
2025-07-18 16:44:34 +0100

    fix

--- src/TransportPlots/Static/Distribution.jl
+++ src/TransportPlots/Static/Distribution.jl
@@ -63,6 +63,13 @@ function MomentumDistributionPlot(sol,species::String,PhaseSpace::PhaseSpaceStru
     t_save = length(sol.t)
     t_plot = ceil(Int64,t_save/step)
 
+    if logt
+        log_t = log10.(sol.t)
+        values = findall(x->x%1==0,log_t)
+    else
+        values = (1:t_save)*step
+    end
+
     for i in 1:t_save
 
         if (i in values || i == 1 || i == 2) # plot first step for initial conds, second for kernel 
@@ -186,6 +193,13 @@ function MomentumDistributionPlot(sol,species::Vector{String},PhaseSpace::PhaseS
 
     t_save = length(sol.t)
     t_plot = ceil(Int64,t_save/step)
+
+    if logt
+        log_t = log10.(sol.t)
+        values = findall(x->x%1==0,log_t)
+    else
+        values = (1:t_save)*step
+    end
 
     for (species_idx, species_name) in enumerate(species) 
 
