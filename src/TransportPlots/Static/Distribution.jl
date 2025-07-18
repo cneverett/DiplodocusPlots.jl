c6d672e0df6bbc879b89dc86c230064ae0a42b9e
Christopher Everett <cn.everett@outlook.com>
2025-07-18 13:45:13 +0100

    distribution plot updates

--- src/TransportPlots/Static/Distribution.jl
+++ src/TransportPlots/Static/Distribution.jl
@@ -8,7 +8,7 @@ Optional arguments:
 - `order`: the order of p in p^order * dN/dp dV, default is 1, i.e. number density spectrum. 2 is "energy" density spectrum.
 - `perp`: interpolates the spherical distribution function to parallel and perpendicular directions and then averages over the parallel direction. (NOT YET IMPLEMENTED CORRECTLY)
 """
-function MomentumDistributionPlot(sol,species::String,PhaseSpace::PhaseSpaceStruct;step=1,order::Int64=1,thermal=false,perp=false,uDis=false,logt=false,plot_limits=(nothing,nothing),theme=DiplodocusDark(),wide=false)
+function MomentumDistributionPlot(sol,species::String,PhaseSpace::PhaseSpaceStruct;step=1,order::Int64=1,thermal=false,perp=false,logt=false,plot_limits=(nothing,nothing),theme=DiplodocusDark(),wide=false)
 
     CairoMakie.activate!(inline=true) # plot in vs code window
 
@@ -175,7 +175,7 @@ function MomentumDistributionPlot(sol,species::String,PhaseSpace::PhaseSpaceStru
 
         MJ = MJ .* (meanp.^(order-1))
 
-        scatterlines!(ax,log10.(meanp),log10.(MJ),linewidth=2.0,color = :white,markersize=0.0,linestyle=:dash,label="Maxwell-Juttner")
+        scatterlines!(ax,log10.(meanp),log10.(MJ),linewidth=2.0,color = theme.textcolor[],markersize=0.0,linestyle=:dash,label="Maxwell-Juttner")
 
     end
 
@@ -196,6 +196,162 @@ function MomentumDistributionPlot(sol,species::String,PhaseSpace::PhaseSpaceStru
 
 end
 
+function MomentumDistributionPlot(sol,species::Vector{String},PhaseSpace::PhaseSpaceStruct;step=1,order::Int64=1,thermal=false,perp=false,logt=false,plot_limits=(nothing,nothing),theme=DiplodocusDark(),wide=false)
+
+    CairoMakie.activate!(inline=true) # plot in vs code window
+
+    with_theme(theme) do
+
+    fig = Figure()
+    if wide
+        fig = Figure(size=(576,216)) # double column 8:3 aspect ratio
+    else
+        fig = Figure() # default single column 4:3 aspect ratio
+    end
+    xlab = L"$\log_{10}\left(p [m_\text{Ele}c]\right)$"
+    if order == 1
+        ylab = L"$\log_{10}\left(p\frac{\mathrm{d}N}{\mathrm{d}p\mathrm{d}V} [\text{m}^{-3}]\right)$"
+    elseif order != 1
+        ylab = L"$\log_{10}\left(p^{%$(order)}\frac{\mathrm{d}N}{\mathrm{d}p\mathrm{d}V} [\text{m}^{-3}\left(m_\text{Ele}c\right)^{%$(order-1)}]\right)$"
+    end
+    ax = Axis(fig[1,1],xlabel=xlab,ylabel=ylab,aspect=DataAspect())
+    ax.limits = plot_limits
+
+    name_list = PhaseSpace.name_list
+    Momentum = PhaseSpace.Momentum
+    Grids = PhaseSpace.Grids
+    Time = PhaseSpace.Time
+
+    linestyles = [:solid,:dash,:dot,:dashdot,:dashdotdot]
+    max_total = -Inf32
+    p_min = Inf32
+    p_max = -Inf32
+
+    legend_element = []
+    line_labels = []
+
+    for (species_idx, species_name) in enumerate(species) 
+
+    species_index = findfirst(x->x==species_name,name_list)
+
+    p_num = Momentum.px_num_list[species_index]  
+    u_num = Momentum.py_num_list[species_index]
+    h_num = Momentum.pz_num_list[species_index]
+    dp = Grids.dpx_list[species_index]
+    du = Grids.dpy_list[species_index]
+    meanp = Grids.mpx_list[species_index]
+    meanu = Grids.mpy_list[species_index]
+    meanh = Grids.mpz_list[species_index]
+    p_r = Grids.pxr_list[species_index]
+    u_r = Grids.pyr_list[species_index]
+    mass = Grids.mass_list[species_index]
+
+    f3D = zeros(Float32,p_num,u_num,h_num)
+    f2D = zeros(Float32,p_num,u_num)
+    f1D = zeros(Float32,p_num)
+
+    p_min = min(p_min,p_r[1])
+    p_max = max(p_max,p_r[end])
+
+    t_save = length(sol.t)
+    t_plot = ceil(Int64,t_save/step)
+
+    color_counter = 1
+
+    if logt
+        log_t = log10.(sol.t)
+        values = findall(x->x%1==0,log_t)
+        my_colors = [cgrad(:rainbow)[z] for z ∈ range(0.0, 1.0, length = length(values)+1)]
+    else
+        values = (1:t_save)*step
+        my_colors = [cgrad(:rainbow)[z] for z ∈ range(0.0, 1.0, length = t_plot+1)]
+    end
+
+    for i in 1:t_save
+
+        if (i in values || i == 1 || i == 2) # plot first step for initial conds, second for kernel 
+
+            t = sol.t[i]
+
+            f3D .= reshape(sol.f[i].x[species_index],(p_num,u_num,h_num))
+
+            max_f = maximum(x for x in f3D if !isnan(x))
+            max_total = max(max_f,max_total)
+
+            @. f3D = f3D*(f3D!=Inf)
+            #scale by order-1
+            for px in 1:p_num, py in 1:u_num, pz in 1:h_num
+                f3D[px,py,pz] = f3D[px,py,pz] * (meanp[px]^(order-1))
+            end
+
+            #=if uDis
+                dj .= log10.(d .* dp)[:,4:3:8] # print just aligned and anti-aligned with field
+
+                scatterlines!(ax,log10.(meanp),dj[:,1],linewidth=2.0,color = my_colors[color_counter],markersize=1.0)
+                scatterlines!(ax,log10.(meanp),dj[:,2],linewidth=2.0,color = my_colors[color_counter],markersize=1.0,linestyle=:dash)=#
+            if perp  
+                f2D .= dropdims(sum(f3D, dims=(3)),dims=(3))
+                if iseven(u_num) 
+                    pdNdp = f2D[:,Int64(u_num/2)]#= /(2*pi*du[Int64(u_num/2)]) =#+f2D[:,Int64(u_num//2+1)]#= /(2*pi*du[Int64(u_num/2+1)]) =#
+                else
+                    pdNdp = f2D[:,Int64((u_num+1)/2)]
+                end
+                scatterlines!(ax,log10.(meanp),log10.(pdNdp),linewidth=2.0,color = my_colors[color_counter],markersize=1.0,linestyle=linestyles[species_idx])
+            else
+                # sum along u and h directions
+                pdNdp = dropdims(sum(f3D, dims=(2,3)),dims=(2,3))
+                scatterlines!(ax,log10.(meanp),log10.(pdNdp),linewidth=2.0,color = my_colors[color_counter],markersize=1.0,linestyle=linestyles[species_idx])
+            end
+
+            color_counter += 1
+
+        end
+
+    end
+
+    if thermal
+
+        # expected thermal spectrum based on final time step
+        f = sol.f[end].x[species_index]
+        Nᵃ = DiplodocusTransport.FourFlow(f,p_num,u_num,p_r,u_r,mass)
+        Uₐ = [-1.0,0.0,0.0,0.0] # static observer
+        num = DiplodocusTransport.ScalarNumberDensity(Nᵃ,Uₐ)
+        Δab = DiplodocusTransport.ProjectionTensor(Uₐ)
+        Tᵃᵇ = DiplodocusTransport.StressEnergyTensor(f,p_num,u_num,p_r,u_r,mass)
+        Pressure = DiplodocusTransport.ScalarPressure(Tᵃᵇ,Δab)
+        Temperature = DiplodocusTransport.ScalarTemperature(Pressure,num)
+
+        MJ = DiplodocusTransport.MaxwellJuttner_Distribution(PhaseSpace,species,Temperature;n=num)
+
+        MJ = MJ .* (meanp.^(order-1))
+
+        scatterlines!(ax,log10.(meanp),log10.(MJ),linewidth=1.0,color = :white,markersize=0.0,label="Maxwell-Juttner")
+
+    end
+
+    push!(legend_element,LineElement(color = theme.textcolor[], linestyle = linestyles[species_idx],linewidth = 2.0))
+    push!(line_labels,species_name)
+
+    end # species loop 
+
+    if Time.t_grid == "u"
+        Colorbar(fig[1,2],colormap = my_colors,limits=(sol.t[1],sol.t[end]),label=L"$t$ $[\text{s} * \sigma_{T}c]$")
+    elseif Time.t_grid == "l"
+        Colorbar(fig[1,2],colormap = my_colors,limits=(log10(sol.t[1]),log10(sol.t[end])),label=L"$\log_{10}\left(t[\text{s} \times \sigma_{T}c]\right)$")
+    end
+
+    axislegend(ax,legend_elements,line_labels)
+
+    if plot_limits == (nothing,nothing)
+        xlims!(ax,(log10.(p_min)-1.0,log10.(p_max)+1.0))
+        ylims!(ax,(max_total-11.0,max_total+1.0)) 
+    end
+    
+    return fig
+
+    end # with_theme
+
+end
 
 """
     MomentumAndPolarAngleDistributionPlot(sol,species,PhaseSpace,time;order=1,theme=DiplodocusDark())
