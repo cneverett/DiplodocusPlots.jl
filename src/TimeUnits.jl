a94436050896ee708d3b942eb368e6a5133957f0
Christopher Everett <cn.everett@outlook.com>
2025-07-18 14:42:34 +0100

    fix

--- src/TimeUnits.jl
+++ src/TimeUnits.jl
@@ -5,10 +5,10 @@ Returns the time in SI units seconds from the time in code units of seconds * σ
 """
 function SIUnitsTime(t_code::T) where T <: AbstractFloat
     # Convert time code to SI units (seconds)
-    σT = getfield(DC,Symbol(σT))
-    c = getfield(DC,Symbol(c))  # σT is the time unit in seconds
+    σT = getfield(DC,Symbol("σT"))
+    c = getfield(DC,Symbol("c"))  # σT is the time unit in seconds
 
-    t_SI::T = t_code * σT  # Convert time code to seconds
+    t_SI::T = t_code * σT * c  # Convert time code to seconds
     return t_SI
 end
 function SIUnitsTime()
