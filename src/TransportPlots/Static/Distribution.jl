b3a3ed699c607eb3c235c7829c62f14be61727fe
Christopher Everett <cn.everett@outlook.com>
2025-07-18 16:38:15 +0100

    fix

--- src/TransportPlots/Static/Distribution.jl
+++ src/TransportPlots/Static/Distribution.jl
@@ -35,6 +35,10 @@ function MomentumDistributionPlot(sol,species::String,PhaseSpace::PhaseSpaceStru
     Grids = PhaseSpace.Grids
     Time = PhaseSpace.Time
 
+    max_total = -Inf32
+    p_min = Inf32
+    p_max = -Inf32
+
     species_index = findfirst(x->x==species,name_list)
 
     p_num = Momentum.px_num_list[species_index]  
@@ -53,66 +57,22 @@ function MomentumDistributionPlot(sol,species::String,PhaseSpace::PhaseSpaceStru
     f2D = zeros(Float32,p_num,u_num)
     f1D = zeros(Float32,p_num)
 
+    p_min = min(p_min,p_r[1])
+    p_max = max(p_max,p_r[end])
+
     t_save = length(sol.t)
-
     t_plot = ceil(Int64,t_save/step)
-
-    color_counter = 1
-
-    if logt
-        log_t = log10.(sol.t)
-        values = findall(x->x%1==0,log_t)
-        my_colors = [cgrad(:rainbow)[z] for z ∈ range(0.0, 1.0, length = length(values)+1)]
-    else
-        values = (1:t_save)*step
-        my_colors = [cgrad(:rainbow)[z] for z ∈ range(0.0, 1.0, length = t_plot+1)]
-    end
-
-    if perp 
-        #=error("Perpendicular not yet implemented correctly")
-        xs = zeros(Float64,p_num,u_num)
-        ys = zeros(Float64,p_num,u_num)
-        for px in 1:p_num, py in 1:u_num
-            xs[px,py] = meanp[px]*sqrt(1-meanu[py]^2)
-            ys[px,py] = meanp[px]*meanu[py]
-        end
-        xs = reshape(xs,(p_num*u_num))
-        ys = reshape(ys,(p_num*u_num))
-        xi = range(meanp[1],meanp[end],length=201)
-        yi = range(-meanp[end],meanp[end],length=401)
-        xid = zeros(Float64,length(xi)-1)
-        yid = zeros(Float64,length(yi)-1)
-        xim = zeros(Float64,length(xi)-1)
-        yim = zeros(Float64,length(yi)-1)
-        r = zeros(Float64,p_num*u_num)
-        rmin_idx = zeros(Int64,length(xi),length(yi))
-        f2D_cart = zeros(Float64,length(xi),length(yi))
-        for x in eachindex(xim), y in eachindex(yim)
-            xim[x] = (xi[x+1] + xi[x])/2
-            xid[x] = (xi[x+1] - xi[x])
-            yim[y] = (yi[y+1] + yi[y])/2
-            yid[y] = (yi[y+1] - yi[y])
-            for i in 1:p_num*u_num
-                r[i] = sqrt((xs[i]-xim[x])^2 + (ys[i]-yim[y])^2)
-            end
-            # find closest
-            rmin_idx[x,y] = findmin(r)[2]
-            # area ratios for correct number density
-            idx = CartesianIndices(f2D)[rmin_idx[x,y]]
-            p = idx[1]
-            u = idx[2]
-            f2D_cart[x,y] = 1.0meanp[p]^2*xim[x]*xid[x]*yid[y]/(dp[p]*du[u])
-        end
-        =#
-    end
-
-    max_total = -Inf32
 
     for i in 1:t_save
 
         if (i in values || i == 1 || i == 2) # plot first step for initial conds, second for kernel 
 
             t = sol.t[i]
+            if Time.t_grid == "u"
+                color = Makie.ColorSchemes.rainbow_bgyr_35_85_c72_n256[(t - sol.t[1]) / (sol.t[end] - sol.t[1])]
+            elseif Time.t_grid == "l"
+                color = Makie.ColorSchemes.rainbow_bgyr_35_85_c72_n256[(log10(t) - log10(sol.t[1])) / (log10(sol.t[end]) - log10(sol.t[1]))]
+            end
 
             f3D .= reshape(sol.f[i].x[species_index],(p_num,u_num,h_num))
 
@@ -128,36 +88,24 @@ function MomentumDistributionPlot(sol,species::String,PhaseSpace::PhaseSpaceStru
                 scatterlines!(ax,log10.(meanp),dj[:,1],linewidth=2.0,color = my_colors[color_counter],markersize=1.0)
                 scatterlines!(ax,log10.(meanp),dj[:,2],linewidth=2.0,color = my_colors[color_counter],markersize=1.0,linestyle=:dash)=#
             if perp  
-                #=f2D .= dropdims(sum(f3D, dims=(2,3)),dims=(2,3))
-                f2D_flat = reshape(f2D,(p_num*u_num))
-                for x in eachindex(xim), y in eachindex(yim)
-                    f2D_cart[x,y] *= f2D_flat[rmin_idx[x,y]]
-                end
-                pdNdp = dropdims(sum(f2D_cart, dims=(2)),dims=(2))
-                for px in 1:length(xim)
-                    pdNdp[px] = pdNdp[px] * (xim[px]^(order-1))
-                end
-                scatterlines!(ax,log10.(xi),log10.(pdNdp),linewidth=2.0,color = my_colors[color_counter],markersize=1.0)=#
                 f2D .= dropdims(sum(f3D, dims=(3)),dims=(3))
                 if iseven(u_num) 
                     pdNdp = f2D[:,Int64(u_num/2)]#= /(2*pi*du[Int64(u_num/2)]) =#+f2D[:,Int64(u_num//2+1)]#= /(2*pi*du[Int64(u_num/2+1)]) =#
                 else
                     pdNdp = f2D[:,Int64((u_num+1)/2)]
                 end
-                scatterlines!(ax,log10.(meanp),log10.(pdNdp),linewidth=2.0,color = my_colors[color_counter],markersize=1.0)
+                scatterlines!(ax,log10.(meanp),log10.(pdNdp),linewidth=2.0,color = color,markersize=0.0)
 
                 max_f = maximum(x for x in pdNdp if !isnan(x))
                 max_total = max(max_f,max_total)
             else
                 # sum along u and h directions
                 pdNdp = dropdims(sum(f3D, dims=(2,3)),dims=(2,3))
-                scatterlines!(ax,log10.(meanp),log10.(pdNdp),linewidth=2.0,color = my_colors[color_counter],markersize=1.0)
+                scatterlines!(ax,log10.(meanp),log10.(pdNdp),linewidth=2.0,color = color,markersize=0.0)
 
                 max_f = maximum(x for x in pdNdp if !isnan(x))
                 max_total = max(max_f,max_total)
             end
-
-            color_counter += 1
 
         end
 
@@ -183,10 +131,12 @@ function MomentumDistributionPlot(sol,species::String,PhaseSpace::PhaseSpaceStru
 
     end
 
+    t_unit_string = TimeUnits()
+
     if Time.t_grid == "u"
-        Colorbar(fig[1,2],colormap = my_colors,limits=(TimeUnits(sol.t[1]),TimeUnits(sol.t[end])),label=L"$t$ $[\text{s} * \sigma_{T}c]$")
+        Colorbar(fig[1,2],colormap = Makie.ColorSchemes.rainbow_bgyr_35_85_c72_n256,limits=(TimeUnits(sol.t[1]),TimeUnits(sol.t[end])),label=L"$t$ $[\text{s} * \sigma_{T}c]$")
     elseif Time.t_grid == "l"
-        Colorbar(fig[1,2],colormap = my_colors,limits=(log10(TimeUnits(sol.t[1])),log10(TimeUnits(sol.t[end]))),label=L"$\log_{10}\left(t[\text{s} \times \sigma_{T}c]\right)$")
+        Colorbar(fig[1,2],colormap = Makie.ColorSchemes.rainbow_bgyr_35_85_c72_n256,limits=(log10(TimeUnits(sol.t[1])),log10(TimeUnits(sol.t[end]))),label=L"$\log_{10}\left(t %$t_unit_string \right)$")
     end
 
     if plot_limits == (nothing,nothing)
@@ -237,15 +187,6 @@ function MomentumDistributionPlot(sol,species::Vector{String},PhaseSpace::PhaseS
     t_save = length(sol.t)
     t_plot = ceil(Int64,t_save/step)
 
-    if logt
-        log_t = log10.(sol.t)
-        values = findall(x->x%1==0,log_t)
-        my_colors = [cgrad(:rainbow)[z] for z ∈ range(0.0, 1.0, length = length(values)+1)]
-    else
-        values = (1:t_save)*step
-        my_colors = [cgrad(:rainbow)[z] for z ∈ range(0.0, 1.0, length = t_plot+1)]
-    end
-
     for (species_idx, species_name) in enumerate(species) 
 
     species_index = findfirst(x->x==species_name,name_list)
@@ -268,8 +209,6 @@ function MomentumDistributionPlot(sol,species::Vector{String},PhaseSpace::PhaseS
 
     p_min = min(p_min,p_r[1])
     p_max = max(p_max,p_r[end])
-
-    color_counter = 1
 
     for i in 1:t_save
 
@@ -302,20 +241,18 @@ function MomentumDistributionPlot(sol,species::Vector{String},PhaseSpace::PhaseS
                 else
                     pdNdp = f2D[:,Int64((u_num+1)/2)]
                 end
-                scatterlines!(ax,log10.(meanp),log10.(pdNdp),linewidth=2.0,color = color,markersize=1.0,linestyle=linestyles[species_idx])
+                scatterlines!(ax,log10.(meanp),log10.(pdNdp),linewidth=2.0,color = color,markersize=0.0,linestyle=linestyles[species_idx])
 
                 max_f = maximum(x for x in pdNdp if !isnan(x))
                 max_total = max(max_f,max_total)
             else
                 # sum along u and h directions
                 pdNdp = dropdims(sum(f3D, dims=(2,3)),dims=(2,3))
-                scatterlines!(ax,log10.(meanp),log10.(pdNdp),linewidth=2.0,color = color,markersize=1.0,linestyle=linestyles[species_idx])
+                scatterlines!(ax,log10.(meanp),log10.(pdNdp),linewidth=2.0,color = color,markersize=0.0,linestyle=linestyles[species_idx])
 
                 max_f = maximum(x for x in pdNdp if !isnan(x))
                 max_total = max(max_f,max_total)
             end
-
-            color_counter += 1
 
         end
 
@@ -354,7 +291,7 @@ function MomentumDistributionPlot(sol,species::Vector{String},PhaseSpace::PhaseS
         Colorbar(fig[1,2],colormap = Makie.ColorSchemes.rainbow_bgyr_35_85_c72_n256,limits=(log10(TimeUnits(sol.t[1])),log10(TimeUnits(sol.t[end]))),label=L"$\log_{10}\left(t %$t_unit_string \right)$")
     end
 
-    axislegend(ax,legend_elements,line_labels,position = :lt,rowgap=-5)
+    axislegend(ax,legend_elements,line_labels,position = :lt)
 
     if plot_limits == (nothing,nothing)
         xlims!(ax,(log10(p_min)-1.0,log10(p_max)+1.0))
