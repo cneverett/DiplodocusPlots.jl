0adaaaa968734439ba9df9760157e06b4f1c868d
Christopher Everett <cn.everett@outlook.com>
2025-07-21 10:54:07 +0100

    added si to code units

--- src/TimeUnits.jl
+++ src/TimeUnits.jl
@@ -1,9 +1,9 @@
 """ 
-    SIUnitsTime(t_code)
+    CodeToSIUnitsTime(t_code)
 
 Returns the time in SI units seconds from the time in code units of seconds * σTc 
 """
-function SIUnitsTime(t_code::T) where T <: AbstractFloat
+function CodeToSIUnitsTime(t_code::T) where T <: AbstractFloat
     # Convert time code to SI units (seconds)
     σT = getfield(DC,Symbol("σT"))
     c = getfield(DC,Symbol("c"))  # σT is the time unit in seconds
@@ -11,18 +11,33 @@ function SIUnitsTime(t_code::T) where T <: AbstractFloat
     t_SI::T = t_code / (σT*c)  # Convert time code to seconds
     return t_SI
 end
-function SIUnitsTime()
+function CoeToSIUnitsTime()
     return raw"$[\text{s}]$"
 end
 
 """ 
-    CodeUnitsTime(t_code)
+    CodeToCodeUnitsTime(t_code)
 
 Returns the time in code units from the time in code units
 """
-function CodeUnitsTime(t_code::T) where T <: AbstractFloat
+function CodeUnitsToCodeUnitsTime(t_code::T) where T <: AbstractFloat
     return t_code
 end
-function CodeUnitsTime()
+function CodeToCodeUnitsTime()
     return raw"$[\text{s} \times \sigma_{T}c]$"
+end
+
+""" 
+    CodeToSIUnitsTime(t_code)
+
+Returns the time in code units seconds from the time in units of seconds
+"""
+function SIToCodeUnitsTime(t_SI::T) where T <: AbstractFloat
+    # Convert time code to SI units (seconds)
+    σT = getfield(DC,Symbol("σT"))
+    c = getfield(DC,Symbol("c"))  # σT is the time unit in seconds
+
+    t_code::T = t_SI * (σT*c)  # Convert time code to seconds
+    return t_code
+    
 end
