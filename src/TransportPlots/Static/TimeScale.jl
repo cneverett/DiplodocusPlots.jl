e35b301256e9ac12e383dfced797109d0566c930
Christopher Everett <cn.everett@outlook.com>
2025-09-05 16:04:55 +0100

    fix?

--- src/TransportPlots/Static/TimeScale.jl
+++ src/TransportPlots/Static/TimeScale.jl
@@ -56,6 +56,7 @@ function TimeScalePlot(method::DiplodocusTransport.SteppingMethodType,state::Vec
         timescale1D = copy(Location_Species_To_StateVector(timescale,PhaseSpace,species_index=species))
         timescale3D = reshape(timescale1D,(p_num,u_num,h_num))
         @. timescale3D = timescale3D*(timescale3D!=Inf)
+        @. timescale3D = timescale3D*(timescale3D!=-Inf)
 
         timescale2D = dropdims(sum(timescale3D, dims=(3)),dims=(3))
 
