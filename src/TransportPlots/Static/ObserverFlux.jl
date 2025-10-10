c8e16aae3f90fd7a04e8acbf28ef15c5f9ed69ae
Christopher Everett <cn.everett@outlook.com>
2025-10-10 10:35:52 +0100

    axis update

--- src/TransportPlots/Static/ObserverFlux.jl
+++ src/TransportPlots/Static/ObserverFlux.jl
@@ -185,7 +185,7 @@ function ObserverFluxPlot(PhaseSpace::PhaseSpaceStruct,sol::OutputStruct,time_id
     xlab = L"$\log_{10}\left(p [m_ec]\right)$"
     xlab_t = L"$\log_{10}\left(\nu [\text{Hz}]\right)$"
     ylab = L"$\log_{10}\left(pF_{p}\,[\text{J}\text{m}^{-2}\text{s}^{-1}]\right)$"
-    ax = Axis(fig[1,1],xlabel=xlab,ylabel=ylab,aspect=DataAspect())
+    ax = Axis(fig[1,1],xlabel=xlab,ylabel=ylab,aspect=DataAspect(),xminorticks=IntervalsBetween(5),yminorticks=IntervalsBetween(2),xminorgridvisible=true,yminorgridvisible=true,xminorticksvisible=true,yminorticksvisible=true)
     ax_t = Axis(fig[1,1],xlabel=xlab_t,xaxisposition=:top,aspect=DataAspect())
     hidespines!(ax_t)
     hidexdecorations!(ax_t,ticklabels=false,ticks=false,label=false,minorgrid=false,minorticks=false)
